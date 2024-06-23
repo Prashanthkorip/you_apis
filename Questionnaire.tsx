82be8662de507b00bb634e35fef0470f320b8565
Prashanthkorip <159862471+Prashanthkorip@users.noreply.github.com>
2024-06-23 11:49:22 -0700

    Add files via upload

---
+++ Questionnaire.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const questions = [
+  "How often do you travel?",
+  "How much trash do you produce?",
+  "How much energy do you consume?",
+  "How much water do you use?",
+  "Do you have plants at home?",
+];
+
+const Questionnaire: React.FC<{
+  setScores: (scores: { [key: string]: number }) => void;
+}> = ({ setScores }) => {
+  const options = (obj: Object) => ({
+    method: "GET",
+    header: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(obj),
+  });
+
+  const mockApiCall = async (answers: string[]) => {
+    const apiResponse = await Promise.all([
+      axios(
+        "http://127.0.0.1:5000/travel",
+        options({
+          "answers1": answers[0]
+      })
+      ),
+      axios(
+        "http://127.0.0.1:5000/trash",
+        options({
+          "answers2":answers[1]
+      })
+      ),
+      axios(
+        "https://chat-api.you.com/energy",
+        options({
+          "answers3":answers[2]
+        })
+      ),
+      axios(
+        "https://chat-api.you.com/water",
+        options({
+          "answers4":answers[3]
+        })
+      ),
+      axios(
+        "https://chat-api.you.com/plants",
+        options({
+          "answers5":answers[4]
+        })
+      ),
+    ]);
+
+    console.log(apiResponse);
+
+    // const travelScore = apiResponse.data.travel || Math.floor(Math.random() * 100);
+
+    return {
+      travel: Math.floor(Math.random() * 100),
+      trash: Math.floor(Math.random() * 100),
+      energy: Math.floor(Math.random() * 100),
+      water: Math.floor(Math.random() * 100),
+      plant: Math.floor(Math.random() * 100),
+    };
+  };
+  const [answers, setAnswers] = useState<string[]>(
+    Array(questions.length).fill("")
+  );
+  const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newAnswers = [...answers];
+    newAnswers[currentQuestion] = e.target.value;
+    setAnswers(newAnswers);
+  };
+
+  const handleSubmit = async () => {
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      const scores = await mockApiCall(answers);
+      setScores(scores);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Carbon Footprint Questionnaire</h1>
+      <p>{questions[currentQuestion]}</p>
+      <input
+        type="text"
+        value={answers[currentQuestion]}
+        onChange={handleChange}
+      />
+      <button onClick={handleSubmit}>Next</button>
+    </div>
+  );
+};
+
+export default Questionnaire;
